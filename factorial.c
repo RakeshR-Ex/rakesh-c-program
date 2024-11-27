fa33fecb7ac964d8f2bb60ab0e5b65516e336ff5
Rakesh R <rakeshrockzz615@gmail.com>
2024-11-27 02:40:43 -0800

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int factorial(int n) {
+  if(n==0) {
+    return 1;
+  
+  }
+  return n*factorial(n-1);
+}
+int main() {
+  int num;
+  printf("enter a number: ");
+  scanf("%d", &num);
+  printf("factorial = %d", factorial(num));
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
