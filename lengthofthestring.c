bce8b8488a76d5165ec240306e8deb8306ef1e55
Rakesh R <rakeshrockzz615@gmail.com>
2024-11-27 22:42:38 -0800

    Add files via upload

---
+++ lengthofthestring.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[10];
+     printf("enter your name: ");
+     scanf("%s", name);
+    int length = strlen(name);
+    printf("Length of the string: %d\n", length);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
