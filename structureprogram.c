aba35ab688c025d33f66d3e58f64e6577aa25b7d
Rakesh R <rakeshrockzz615@gmail.com>
2024-11-29 10:31:28 +0530

    Add files via upload

---
+++ structureprogram.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct student {
+char name[50];
+int roll;
+float marks;
+};
+int main() {
+  struct student s1;
+  printf("enter the name: ");
+  scanf("%s", s1.name);
+  printf("enter roll number: ");
+  scanf("%d", &s1.roll);
+  printf("enter marks: ");
+  scanf("%f", &s1.marks);
+  printf("\nstudent details:\n");
+  printf("Name: %s\nROll: %d\nMarks: %.2f\n", s1.name, s1.roll, s1.marks);
+  return 0;
+}
